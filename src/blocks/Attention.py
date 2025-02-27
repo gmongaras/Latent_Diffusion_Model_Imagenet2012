Fixed norm issue in softmax attn

src/blocks/Attention.py
@@ -6,11 +6,12 @@ from src.blocks.rotary_embedding import RotaryEmbedding
 
 
 class Attention(nn.Module):
-    def __init__(self, dim, num_heads = 8, causal=False, emb_dim=None, positional_encoding="absolute", layer_idx=None):
+    def __init__(self, dim, num_heads = 8, causal=False, emb_dim=None, positional_encoding="absolute", layer_idx=None, legacy_norm=False):
         super().__init__()
 
         self.layer_idx = layer_idx
         self.positional_encoding = positional_encoding
+        self.legacy_norm = legacy_norm
         
         # Projections
         self.query_proj = nn.Linear(dim, dim if emb_dim == None else emb_dim, bias = False)
@@ -25,9 +26,14 @@ class Attention(nn.Module):
         self.scale = self.head_dim ** -0.5
 
         # Softmax attention also needs q k norms
-        self.q_norm = nn.RMSNorm(dim, dim)
-        self.k_norm = nn.RMSNorm(dim, dim)
-
+        if legacy_norm:
+            # I messed this up here
+            self.q_norm = nn.RMSNorm(dim, dim)
+            self.k_norm = nn.RMSNorm(dim, dim)
+        else:
+            # I messed this up here
+            self.q_norm = nn.RMSNorm(self.head_dim)
+            self.k_norm = nn.RMSNorm(self.head_dim)
 
         self.causal = causal
 
@@ -46,8 +52,12 @@ class Attention(nn.Module):
 
 
         # RMSNorm and Project the queries, keys, and values (N, C, d) --> (N, H, C, d//H)
-        queries = self.q_norm(self.query_proj(x)).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
-        keys = self.k_norm(self.key_proj(x)).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        if self.legacy_norm:
+            queries = self.q_norm(self.query_proj(x)).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+            keys = self.k_norm(self.key_proj(x)).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        else:
+            queries = self.q_norm(self.query_proj(x).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3))
+            keys = self.k_norm(self.key_proj(x).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3))
         values = self.value_proj(x).reshape(N, C, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
 
 

src/blocks/Transformer_Block.py
@@ -12,13 +12,13 @@ from xformers.ops.swiglu_op import SwiGLU
 
 
 class Transformer_Block(nn.Module):
-    def __init__(self, dim, c_dim, hidden_scale=4.0, num_heads = 8, causal=False, positional_encoding="absolute", layer_idx=None):
+    def __init__(self, dim, c_dim, hidden_scale=4.0, num_heads = 8, causal=False, positional_encoding="absolute", layer_idx=None, legacy_norm=False):
         super().__init__()
         
         # MLP and attention blocks
         self.MLP = MLP(dim, hidden_scale)
         # self.MLP = SwiGLU(dim, int(dim*hidden_scale), dim)
-        self.attn = Attention(dim, num_heads=num_heads, causal=causal, positional_encoding=positional_encoding, layer_idx=layer_idx)
+        self.attn = Attention(dim, num_heads=num_heads, causal=causal, positional_encoding=positional_encoding, layer_idx=layer_idx, legacy_norm=legacy_norm)
         
         # Two layer norms
         self.norm1 = Norm(dim, c_dim)

src/models/diff_model.py
@@ -37,7 +37,7 @@ class diff_model(nn.Module):
     # device - Device to put the model on (gpu or cpu)
     # start_step - Step to start on. Doesn't do much besides 
     #               change the name of the saved output file
-    def __init__(self, inCh, num_classes, patch_size, dim, c_dim, hidden_scale, num_heads, num_blocks, positional_encoding, device, start_step=0, wandb_id=None):
+    def __init__(self, inCh, num_classes, patch_size, dim, c_dim, hidden_scale, num_heads, num_blocks, positional_encoding, device, start_step=0, wandb_id=None, legacy_norm=False):
         super(diff_model, self).__init__()
         
         self.inCh = inCh
@@ -45,6 +45,9 @@ class diff_model(nn.Module):
         self.patch_size = patch_size
         self.start_step = start_step
         self.wandb_id = wandb_id
+
+        # I messed up the norm when pretraining
+        self.legacy_norm = legacy_norm
         
         # Important default parameters
         self.defaults = {
@@ -60,6 +63,7 @@ class diff_model(nn.Module):
             "device": "cpu",
             "start_step": start_step,
             "wandb_id": wandb_id,
+            "legacy_norm": self.legacy_norm
         }
         
         # Convert the device to a torch device
@@ -87,7 +91,7 @@ class diff_model(nn.Module):
         
         # Transformer blocks
         self.blocks = nn.ModuleList([
-            Transformer_Block(dim, c_dim=dim, hidden_scale=hidden_scale, num_heads=num_heads, positional_encoding=positional_encoding, layer_idx=i).to(device)
+            Transformer_Block(dim, c_dim=dim, hidden_scale=hidden_scale, num_heads=num_heads, positional_encoding=positional_encoding, layer_idx=i, legacy_norm=self.legacy_norm).to(device)
             for i in range(num_blocks)
         ])
             